401f6cfd2c25e05e8883c988b62e7108cf8f66dc
lncodes <lastnightcodes@gmail.com>
2020-11-21 16:50:57 +0700

    Squashed 'PBO10K-10119907-Latihan51-GajiKaryawan/' content from commit 1b11393
    git-subtree-dir: PBO10K-10119907-Latihan51-GajiKaryawan
    git-subtree-split: 1b11393de869207f75d41f44bbeefa3b4d13f75f

---
+++ src/main/java/id/tdoni/gajikaryawan/Karyawan.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.tdoni.gajikaryawan;
+
+/**
+ *
+ * @author ACER
+ */
+public class Karyawan {
+
+    protected String nik;
+    protected String nama;
+    protected String jabatan;
+    protected int golongan;
+    
+    public void setNik(String nik) {
+        this.nik = nik;
+    }
+    
+    public String getNik() {
+        return nik;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJabatan() {
+        return jabatan;
+    }
+
+    public void setJabatan(String jabatan) {
+        this.jabatan = jabatan;
+    }
+
+    public int getGolongan() {
+        return golongan;
+    }
+
+    public void setGolongan(int golongan) {
+        this.golongan = golongan;
+    }
+}
